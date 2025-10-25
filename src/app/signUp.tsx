41de6d0b7f4e68e4cf863ff587a5195779670261
Leonardo Morari dos Santos <morarileonardo269@gmail.com>
2025-10-25 08:49:57 -0300

    Alterado designe da tela de login

--- src/app/signUp.tsx
+++ src/app/signUp.tsx
@@ -36,7 +36,7 @@ export default function Login() {
   }
   return (
 
-    <SafeAreaView className="flex-1 ">
+    <SafeAreaView className="flex-1 bg-blue-950 ">
       <LoginMasthead title="Sobre"
         image={require('../../assets/images/tomato.png')}
       />
@@ -94,12 +94,12 @@ export default function Login() {
           {errors.confirmPassword && <Text>{errors.confirmPassword.message}</Text>}
         </View>
         <View className="flex-row items-center justify-center">
-          <TouchableOpacity onPress={handleSubmit(onSignUp)} className="bg-blue-600 w-52 h-12 flex-row items-center justify-center rounded-full gap-2 mt-8">
+          <TouchableOpacity onPress={handleSubmit(onSignUp)} className="bg-blue-600 w-64 h-12 flex-row items-center justify-center rounded-full gap-2 mt-8">
             <Text className="text-white font-bold text-lg">Criar Conta</Text>
           </TouchableOpacity>
         </View>
         <View className="flex-row items-center justify-center">
-          <TouchableOpacity onPress={() => router.navigate("/login")} className="bg-blue-300 w-52 h-12 flex-row items-center justify-center rounded-full gap-2 mt-8">
+          <TouchableOpacity onPress={() => router.navigate("/login")} className="bg-blue-400 w-64 h-12 flex-row items-center justify-center rounded-full gap-2 mt-8">
             <Text className="text-white font-bold text-lg">Login</Text>
           </TouchableOpacity>
         </View>
